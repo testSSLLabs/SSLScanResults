6405b85fca0e708796cbcb782ff1cecd3cd9b155
Manoj Kumar <manjku@fb.com>
2022-08-02 14:45:23 +0100

    making receiver email address mandatory for the tool argument

--- sslscanresults/argpare_helper.py
+++ sslscanresults/argpare_helper.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--re', type=str, help="email of the receiver to which reports are sent")
+parser.add_argument('--re', type=str, help="email of the receiver to which reports are sent", required=True)
 parser.add_argument('--domain_yaml', type=str, help="Location of custom domain names yaml file")
 parser.add_argument('--local', action='store_true', help="Use local script directory for getting domain names and saving the reports")
 args = parser.parse_args()
